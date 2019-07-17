16798f5a7428f0dd4f9ce09e7f5fc4f5979898cb
vishal <vpatel@throughouttechnologies.com>
2019-07-17 06:24:35 +0530

    add new action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	def login
+		
+	end
+
 end
